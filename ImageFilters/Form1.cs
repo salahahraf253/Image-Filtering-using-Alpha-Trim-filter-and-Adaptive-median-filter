c896368c48a1b270092821b2e80dc417fc052a58
salahahraf253 <73083810+salahahraf253@users.noreply.github.com>
2022-01-02 19:16:00 +0200

    Update Form1.cs
    this class contains the gui of the project and function of display graph

--- ImageFilters/Form1.cs
+++ ImageFilters/Form1.cs
@@ -18,7 +18,7 @@ namespace ImageFilters
         }
 
         public static  byte[,] ImageMatrix;
-
+        byte[,] imageForFiltering;
         private void btnOpen_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
@@ -27,11 +27,13 @@ namespace ImageFilters
                 //Open the browsed image and display it
                 string OpenedFilePath = openFileDialog1.FileName;
                 ImageMatrix = ImageOperations.OpenImage(OpenedFilePath);
+                imageForFiltering = ImageMatrix;
                 ImageOperations.DisplayImage(ImageMatrix, pictureBox1);
-                // Console.WriteLine("Iamge is uploaded!");
             }
         }
         
+
+        //start of code//////////////////////////////////////////////////////////////////////////////////////////////////////////
         private void applyFilterButton(object sender, EventArgs e) //apply button 
         {
             MessageBox.Show("You press apply", "Information");
@@ -51,7 +53,8 @@ namespace ImageFilters
             //4 Min & Max Heap
             //5 Modified Bubble Sort
 
-            if ( FilterBox.Text.Equals("Alpha-Trim Filter")) filterType = 1;
+            if (FilterBox.Text.Equals("Alpha-Trim Filter")) filterType = 1;
+            else if (FilterBox.Text.Equals("Bonus")) filterType = 3;
             else filterType = 2;
             if (SortAlgoCombo.Text.Length==0)
             {
@@ -84,9 +87,6 @@ namespace ImageFilters
                     
                 }
                 byte[,] imageAfterFiltering = ImageOperations.ImageFilter(ImageMatrix, w, sortType, filterType , K); //new image 
-                //display image after processing 
-                // Console.WriteLine("Before");
-                // Console.WriteLine("After");
                 ImageOperations.DisplayImage(imageAfterFiltering, pictureBox2);
                 
             }
@@ -97,7 +97,6 @@ namespace ImageFilters
             }
         }
 
-
         private void displayMedainGraph(object sender, EventArgs e)        //display median graph
         {
             MessageBox.Show("You press on Median Graph button", "Information");
@@ -106,12 +105,12 @@ namespace ImageFilters
             {
                 MessageBox.Show("Please enter a Maximum window size for graph", "Error");
             }
-
             int Wmax = int.Parse(WmaxSize.Text);
             int N = (Wmax - 3) / 2 + 1;
             double[] time_of_quick_sort = new double[N];
             double[] window_size = new double[N];
             double[] time_of_counting_sort = new double[N];
+
             //time_of_quick_sort[0] = 0; window_size[0] = 0; time_of_counting_sort[0] = 0;
 
             double timeBeforeFiltering, timeAfterFiltering;
@@ -121,9 +120,9 @@ namespace ImageFilters
             {
                 window_size[index] = w;
                 timeBeforeFiltering = System.Environment.TickCount;
-                ImageOperations.ImageFilter(ImageMatrix, w, 1, 2, 0);//quick sort 
+                ImageOperations.ImageFilter(imageForFiltering, w, 1, 2, 0);   //quick sort 
                 timeAfterFiltering =System.Environment.TickCount;
-                time_of_quick_sort[index] = (timeAfterFiltering - timeBeforeFiltering);
+                time_of_quick_sort[index] = Math.Abs(timeAfterFiltering - timeBeforeFiltering);
 
                 index++;
             }
@@ -132,19 +131,18 @@ namespace ImageFilters
             for (int w = 3; w <= Wmax; w += 2)
             {
                 timeBeforeFiltering = System.Environment.TickCount;
-                ImageOperations.ImageFilter(ImageMatrix, w, 2, 2, 0);//counting sort 
+                ImageOperations.ImageFilter(imageForFiltering, w, 2, 2, 0);   //counting sort 
                 timeAfterFiltering = System.Environment.TickCount;
-                time_of_counting_sort[index++] = (timeAfterFiltering - timeBeforeFiltering);
+                time_of_counting_sort[index++] = Math.Abs(timeAfterFiltering - timeBeforeFiltering);
             }
 
             //Create a graph and add two curves to it
-            ZGraphForm ZGF = new ZGraphForm("Sample Graph of Median Filter using Counting sort & Quick sort", "N", "f(N)");
+            ZGraphForm ZGF = new ZGraphForm("Sample Graph of Median Filter using Counting sort & Quick sort", "Window Size", "Time in ms");
             
             ZGF.add_curve("Quick sort ", window_size, time_of_quick_sort,Color.Red);            //for quick sort          
             ZGF.add_curve("Counting sort ", window_size, time_of_counting_sort, Color.Blue);    //for counting sort
             ZGF.Show();
         }
-
 
         private void displayAlpaTrimGraph(object sender, EventArgs e)     //alpha trim graph 
         {
@@ -161,9 +159,9 @@ namespace ImageFilters
 
             int Wmax = int.Parse(WmaxSize.Text);
             int N = (Wmax - 3) / 2 + 1;
-            double[] time_of_counting_sort = new double[N];     //counting sort 
-            double[] window = new double[N];                    //window
-            double[] time_of_select_Kth = new double[N];        //select the Kth element 
+            double[] time_of_counting_sort = new double[N];              //counting sort 
+            double[] window = new double[N];                          //window
+            double[] time_of_bubble = new double[N];              // bubble sort
             double timeBeforeFiltering, timeAfterFiltering;
             int index = 0;
             for (int w = 3; w <= Wmax; w += 2)
@@ -172,7 +170,7 @@ namespace ImageFilters
                 try
                 {
                     timeBeforeFiltering = System.Environment.TickCount;
-                    ImageOperations.ImageFilter(ImageMatrix, w, 2, 1, T);// counting sort 
+                    ImageOperations.ImageFilter(imageForFiltering, w, 2, 1, T);           // counting sort 
                     timeAfterFiltering = System.Environment.TickCount;
                     time_of_counting_sort[index++] = (timeAfterFiltering - timeBeforeFiltering);
                 }
@@ -188,9 +186,9 @@ namespace ImageFilters
                 try
                 {
                     timeBeforeFiltering = System.Environment.TickCount;
-                    ImageOperations.ImageFilter(ImageMatrix, w, 3, 1, T);// select the Kth 
+                    ImageOperations.ImageFilter(imageForFiltering, w,5, 1, T);           // Bubble sort
                     timeAfterFiltering = System.Environment.TickCount;
-                    time_of_select_Kth[index++] = (timeAfterFiltering - timeBeforeFiltering);
+                    time_of_bubble[index++] = (timeAfterFiltering - timeBeforeFiltering);
                 }
                 catch (Exception ex)
                 {
@@ -198,67 +196,14 @@ namespace ImageFilters
                 }
             }
             //Create a graph and add two curves to it
-            ZGraphForm ZGF = new ZGraphForm("Sample Graph of Alpha trim using Counting sort & Select the Kth element", "window size", "time in ms");
-            ZGF.add_curve("Counting Sort", window, time_of_counting_sort, Color.Red);           //for quick sort 
-            ZGF.add_curve("Select the Kth element", window, time_of_select_Kth, Color.Blue);    //for counting sort
+            ZGraphForm ZGF = new ZGraphForm("Sample Graph of Alpha trim using Counting sort & Modified Bubble Sort", "window size", "time in ms");
+            ZGF.add_curve("Counting Sort", window, time_of_counting_sort, Color.Red);           //for couting sort
+            ZGF.add_curve("Modified Bubble Sort", window, time_of_bubble, Color.Blue);                            //for Heap
             ZGF.Show();
         }
-
-
-        private void applyHeapVsSelectGraph(object sender, EventArgs e) // Heap vs Select the Kth 
-        {
-            int T = int.Parse(Trim_value.Text);
-
-            int Wmax = int.Parse(WmaxSize.Text);
-            int N = (Wmax - 3) / 2 + 1;
-            double[] time_of_heap = new double[N];     //counting sort 
-            double[] window = new double[N];                    //window
-            double[] time_of_select_Kth = new double[N];        //select the Kth element 
-            double timeBeforeFiltering, timeAfterFiltering;
-            int index = 0;
-            for (int w = 3; w <= Wmax; w += 2)
-            {
-                window[index] = w;
-                try
-                {
-                    timeBeforeFiltering = System.Environment.TickCount;
-                    ImageOperations.ImageFilter(ImageMatrix, w, 4, 1, T);       // Heap
-                    timeAfterFiltering = System.Environment.TickCount;
-                    time_of_heap[index++] = (timeAfterFiltering - timeBeforeFiltering);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error");
-                }
-            }
-
-            index = 0;
-            for (int w = 3; w <= Wmax; w += 2)
-            {
-                try
-                {
-                    timeBeforeFiltering = System.Environment.TickCount;
-                    ImageOperations.ImageFilter(ImageMatrix, w, 2, 1, T);       // select the Kth 
-                    timeAfterFiltering = System.Environment.TickCount;
-                    time_of_select_Kth[index++] = (timeAfterFiltering - timeBeforeFiltering);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error");
-                }
-            }
-            //Create a graph and add two curves to it
-            //ZGraphForm ZGF = new ZGraphForm("Sample Graph of Alpha trim using Heap & Select the Kth element", "N", "f(N)");
-            ZGraphForm ZGF = new ZGraphForm("Sample Graph of Alpha trim using Heap & Counting", "N", "f(N)");
-            ZGF.add_curve("Min & Max Heap", window, time_of_heap, Color.Red);           //for quick sort 
-            //ZGF.add_curve("Select the Kth element", window, time_of_select_Kth, Color.Blue);    //for counting sort
-            ZGF.add_curve("Counting", window, time_of_select_Kth, Color.Blue);    //for counting sort
-            ZGF.Show();
-        }
-
         private void plotGraphBtn(object sender, EventArgs e)
         {
-            MessageBox.Show("You press on Median Graph button", "Information");
+            MessageBox.Show("You press on Alpha Graph button", "Information");
 
             if (WmaxSize.Text.Length == 0)
             {
@@ -266,11 +211,18 @@ namespace ImageFilters
             }
             int trimValue= int.Parse(Trim_value.Text);
             int sortType1=2 , sortType2 =1;
-            string sortName = "";
-            if (comboBox2.Text.Equals("Counting & Bubble sort")) { sortType2 = 5; sortName = "Modified Bubble Sort"; }
-            else if (comboBox2.Text.Equals("Counting & Select the Kth")) {sortType2 = 3; sortName = "Select the Kth"; }
-            else if (comboBox2.Text.Equals("Counting & Heap")) { sortType2 = 4; sortName = "Heap"; }
-
+            string sortName2 = "", sortName1 = "Counting sort";
+            if (comboBox2.Text.Equals("Counting & Bubble sort")) { sortType2 = 5; sortName2 = "Modified Bubble Sort"; }
+            else if (comboBox2.Text.Equals("Counting & Select the Kth")) {sortType2 = 3; sortName2 = "Select the Kth"; }
+            else if (comboBox2.Text.Equals("Counting & Heap")) { sortType2 = 4; sortName2 = "Heap"; }
+            else if(comboBox2.Text.Equals("Bubble Sort & Heap"))
+            {
+                sortName1 = "Bubble sort";
+                sortType1 = 5;
+                sortName2 = "Heap";
+                sortType2 = 4;
+                
+            }
             int Wmax = int.Parse(WmaxSize.Text);
             int N = (Wmax - 3) / 2 + 1;
             double[] timeOfSort1 = new double[N];
@@ -285,7 +237,7 @@ namespace ImageFilters
             {
                 window_size[index] = w;
                 timeBeforeFiltering = System.Environment.TickCount;
-                ImageOperations.ImageFilter(ImageMatrix, w, sortType1, 1, trimValue);       //counting sort 
+                ImageOperations.ImageFilter(imageForFiltering, w, sortType1, 1, trimValue);       //counting sort 
                 timeAfterFiltering = System.Environment.TickCount;
                 timeOfSort1[index] = (timeAfterFiltering - timeBeforeFiltering);
 
@@ -296,16 +248,91 @@ namespace ImageFilters
             for (int w = 3; w <= Wmax; w += 2)
             {
                 timeBeforeFiltering = System.Environment.TickCount;
-                ImageOperations.ImageFilter(ImageMatrix, w, sortType2, 1, trimValue);
+                ImageOperations.ImageFilter(imageForFiltering, w, sortType2, 1, trimValue);
                 timeAfterFiltering = System.Environment.TickCount;
                 timeOfSort2[index++] = (timeAfterFiltering - timeBeforeFiltering);
             }
 
             //Create a graph and add two curves to it
-            ZGraphForm ZGF = new ZGraphForm("Sample Graph of Alpha Trim Filter using Counting sort & " + sortName, "N", "f(N)"); ;
+            ZGraphForm ZGF = new ZGraphForm("Graph of Alpha Trim Filter using "+sortName1+" & " + sortName2, "Window size", "Time in milleSeconds"); ;
 
-            ZGF.add_curve(sortName, window_size, timeOfSort2, Color.Red);            //for second sort          
-            ZGF.add_curve("Counting sort ", window_size, timeOfSort1, Color.Blue);    //for counting sort
+            ZGF.add_curve(sortName2, window_size, timeOfSort2, Color.Red);            //for second sort          
+            ZGF.add_curve(sortName1, window_size, timeOfSort1, Color.Blue);           //for first sort
+            ZGF.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plotAllGraphsOfAlphaTrimFilter(object sender, EventArgs e)
+        {
+            int T = int.Parse(Trim_value.Text);
+
+            int Wmax = int.Parse(WmaxSize.Text);
+            int N = (Wmax - 3) / 2 + 1;
+            double[] time_of_counting_sort = new double[N];     //counting sort 
+            double[] window = new double[N];                    //window
+            double[] time_of_bubble = new double[N];        
+            double[] time_of_Heap = new double[N];
+            double[] time_of_select_th_Kth_element = new double[N];        
+
+            double timeBeforeFiltering, timeAfterFiltering;
+            int index = 0;
+            for (int w = 3; w <= Wmax; w += 2)
+            {
+                window[index] = w;
+                try
+                {
+                    timeBeforeFiltering = System.Environment.TickCount;
+                    ImageOperations.ImageFilter(imageForFiltering, w, 2, 1, T);           // counting sort 
+                    timeAfterFiltering = System.Environment.TickCount;
+                    time_of_counting_sort[index++] = (timeAfterFiltering - timeBeforeFiltering);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+            index = 0;
+            for(int w = 3; w <= Wmax; w += 2)
+            {
+                timeBeforeFiltering = System.Environment.TickCount;
+                ImageOperations.ImageFilter(imageForFiltering, w, 4, 1, T);           //Heap
+                timeAfterFiltering = System.Environment.TickCount;
+                time_of_Heap[index++] = (timeAfterFiltering - timeBeforeFiltering);
+            }
+
+            index = 0;
+            for (int w = 3; w <= Wmax; w += 2)
+            {
+                timeBeforeFiltering = System.Environment.TickCount;
+                ImageOperations.ImageFilter(imageForFiltering, w, 3, 1, T);           //select 
+                timeAfterFiltering = System.Environment.TickCount;
+                time_of_select_th_Kth_element[index++] = (timeAfterFiltering - timeBeforeFiltering);
+            }
+            index = 0;
+            for (int w = 3; w <= Wmax; w += 2)
+            {
+                try
+                {
+                    timeBeforeFiltering = System.Environment.TickCount;
+                    ImageOperations.ImageFilter(imageForFiltering, w, 5, 1, T);           // Bubble sort
+                    timeAfterFiltering = System.Environment.TickCount;
+                    time_of_bubble[index++] = (timeAfterFiltering - timeBeforeFiltering);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+            //Create a graph and add two curves to it
+            ZGraphForm ZGF = new ZGraphForm("Sample Graph of Alpha trim using Counting sort & Modified Bubble Sort", "window size", "time in ms");
+            ZGF.add_curve("Counting Sort", window, time_of_counting_sort, Color.Red);           //for couting sort
+            ZGF.add_curve("Modified Bubble Sort", window, time_of_bubble, Color.Blue);                            //for Heap
+            ZGF.add_curve("Heap", window, time_of_Heap, Color.Black);                            
+            ZGF.add_curve("Select the Kth", window, time_of_select_th_Kth_element, Color.Green);                            
             ZGF.Show();
         }
     }
